efccf85f9e300cb36cf283fa799c8b000e717b0a
chandruapl84-ops <chandruapl84@icloud.com>
2026-07-24 01:09:10 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        let maxCount = 0;
+        let currentCount = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] === 1) {
+                currentCount++;
+                if (currentCount > maxCount) {
+                    maxCount = currentCount;
+                }
+            } else {
+                currentCount = 0;
+            }
+        }
+
+        return maxCount;
+    }
+}
